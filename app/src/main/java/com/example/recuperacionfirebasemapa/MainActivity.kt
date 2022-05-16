46be517b012a92a0c3cbeaa7b6fc83c6b0fd6f13
Juan <jvazquezveloso@danielcastelao.org>
2022-05-16 10:49:08 +0200

    Interfaz de inicio de sesión

--- app/src/main/java/com/example/recuperacionfirebasemapa/MainActivity.kt
+++ app/src/main/java/com/example/recuperacionfirebasemapa/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.recuperacionfirebasemapa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        private lateinit var auth: FirebaseAuth
+        auth = Firebase.auth
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = Firebase.auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
     }
 }
